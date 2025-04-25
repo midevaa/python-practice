9fe5141e84d17b1843bd670887f83370f0606b0e
natalia malwa <natmalwa77@gmail.com>
2025-04-25 18:39:39 +0300

    computes sum of digits in numbers

--- Sum_digits.py
+++ Sum_digits.py
@@ -1,10 +1,9 @@
 #define function sum digits
 def sum_digits(num):
+    #each int in the digit str(num) converts digits into string to get each number in the digit
     return sum(int(digit) for digit in str(num))
-
 # prompt to input a number
 num=int(input("Enter a number: "))
 result=sum_digits(num)
-
 # displays sum of digits
 print("sum of digits:", result)
