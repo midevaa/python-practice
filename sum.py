7c0f39cf6b80f04c158ef9e7652635d44bf18705
natalia malwa <natmalwa77@gmail.com>
2025-04-25 14:26:25 +0300

    sum of all elements

---
+++ sum.py
@@ -0,0 +1,6 @@
+#created a list of numbers
+my_list = [4,20,33,19,50]
+#created variable total which is the sum of all the elements under my_list
+total=sum(my_list)
+#displaying the output
+print(total)
